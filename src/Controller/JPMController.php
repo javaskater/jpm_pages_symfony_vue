768b27c6e26046edc9fa4406590087163875db6c
Jean-Pierre MENA <jpm@jpmena.eu>
2025-11-11 23:41:33 +0100

    Working with Monolog OK

--- src/Controller/JPMController.php
+++ src/Controller/JPMController.php
@@ -7,15 +7,28 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
+use Psr\Log\LoggerInterface;
+
 class JPMController extends AbstractController
 {
+
+    
+    public function __construct(
+        private LoggerInterface $phpLogger,
+    ) {
+    }
 
     #[Route('/', name: 'app_j_p_m')]
     public function index(Request $request): Response
     {
-        return $this->render('jpm/index.html.twig', [
-            'nom' => "Jean-Pierre MENA",
+        $this->phpLogger->debug("[main Controller] Calling jpm");
+        
+        /*return $this->render('jpm/index.html.twig', [
+         #   'nom' => "Jean-Pierre MENA",
 			
-        ]);
+        ]);*/
+        return new Response(
+            'OK'
+        );
     }
 }
